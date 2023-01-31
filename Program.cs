235830700a009f197932120e5b0de54cd9bac4ac
Nachiket Dabhade <dabhadenachiket@gmail.com>
2023-01-31 00:40:25 -0500

    Use Counters to track pairs
    ZeroCount counts the number of cars going east
    PairCount tracks the number of cars that are going east before this car

--- Program.cs
+++ Program.cs
@@ -6,12 +6,32 @@ namespace Question3
     {
         static void Main(string[] args)
         {
-
+            int[] arr = new int[] { 1, 0, 0, 1, 1, 0, 1 };
+            Console.WriteLine(Solution(arr));
         }
 
-        public int Solution(int[] A)
+        public static int Solution(int[] A)
         {
+            int zeroCount = 0;
+            int pairCount = 0;
 
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == 0)
+                {
+                    zeroCount++;
+                }
+                else
+                {
+                    pairCount += zeroCount;
+                }
+                if (pairCount > 1000000000)
+                {
+                    pairCount = -1;
+                }
+            }
+
+            return pairCount;
         }
     }
 }
